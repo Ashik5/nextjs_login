54f7ffae21f7f1b1386607b569201f0952516d35
Ashik5 <48068370+Ashik5@users.noreply.github.com>
2021-12-06 01:21:44 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 module.exports = {
 
-  purge: [
-
-    './src/**/*.html',
-
-    './src/**/*.js',
-
-  ],
+  purge: ['./src/components/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
    darkMode: false, // or 'media' or 'class'
    theme: {
      extend: {},
